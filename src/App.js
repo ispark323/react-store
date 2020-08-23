a3b9e2c7d1a6cb7016b583560285262b2918fc7a
Park <sungmin.park@rakuten.com>
2020-08-23 17:19:08 +0900

    added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+
+import HomePage from './homepage.component';
+
 import './App.css';
 
-function App() {
-  return <div>React Store</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
